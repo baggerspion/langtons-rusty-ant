e4d0942baff16cbb59104cae0b99cacd346040b6
Paul Adams <paul@baggerspion.net>
2021-09-16 09:53:16 +0200

    Cleaner

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 mod ant;
 mod game;
 
-use std::{thread, time};
+use std::{thread, time::Duration};
 
 const WIDTH: usize = 20;
 const HEIGHT: usize = 20;
-const SLEEP: time::Duration = time::Duration::from_millis(1000);
+const SLEEP: Duration = Duration::from_millis(1000);
 
 fn main() {
     let mut game = game::Game::new(WIDTH, HEIGHT);
